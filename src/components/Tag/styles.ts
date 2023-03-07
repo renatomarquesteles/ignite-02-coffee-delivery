1b50bf312c45db505c83befd0566e9c3a1c25134
Renato <renatomarquesteles@gmail.com>
2023-03-07 17:22:08 -0300

    feat: adds normal Tag component

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -13,7 +13,15 @@ const BaseContainer = styled.div<BaseContainerProps>`
   background: ${(props) => props.theme[props.color]};
 `;
 
-export const Container = styled(BaseContainer)``;
+export const Container = styled(BaseContainer)`
+  color: ${(props) => props.theme["yellow-dark"]};
+  padding: 0.25rem 0.5rem;
+  text-transform: uppercase;
+  font-size: 0.625rem;
+  font-weight: 700;
+  line-height: 130%;
+  border-radius: 6.25rem;
+`;
 
 export const IconContainer = styled(BaseContainer)`
   width: fit-content;
