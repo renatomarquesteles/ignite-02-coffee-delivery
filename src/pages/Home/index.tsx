6a00c6b9fcf674cac93a5538229525debeaa8829
Renato <renatomarquesteles@gmail.com>
2023-03-07 17:22:34 -0300

    feat: adds Coffees list components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,33 @@
-import { Coffee, MapPin, Package, ShoppingCart, Timer } from "phosphor-react";
+import {
+  Coffee,
+  MapPin,
+  Package,
+  ShoppingCart,
+  ShoppingCartSimple,
+  Timer,
+} from "phosphor-react";
 
+import coffee1 from "../../assets/coffees/1.png";
 import coffeeCupImg from "../../assets/coffee-cup.png";
 import logoImg from "../../assets/logo.svg";
 import { CartButton } from "../../components/CartButton";
 import { Tag } from "../../components/Tag";
+import { QuantityInput } from "../../components/QuantityInput";
+import { IconButton } from "../../components/IconButton";
 
-import { Benefits, Header, Info, Introduction, Location, Page } from "./styles";
+import {
+  AddToCart,
+  AddToCartWrapper,
+  Benefits,
+  CoffeeCard,
+  CoffeeList,
+  Header,
+  Info,
+  Introduction,
+  Location,
+  Menu,
+  Page,
+} from "./styles";
 
 export const Home = () => {
   return (
@@ -65,6 +87,30 @@ export const Home = () => {
           </div>
           <img src={coffeeCupImg} alt="" />
         </Introduction>
+
+        <Menu>
+          <h1>Menu</h1>
+          <CoffeeList>
+            {Array.from({ length: 5 }, (_, index) => (
+              <CoffeeCard key={index}>
+                <img src={coffee1} alt="" />
+                <Tag>Traditional</Tag>
+                <h1>Traditional Espresso</h1>
+                <p>Traditional coffee made from hot water and ground beans</p>
+                <AddToCart>
+                  <span>$9.90</span>
+
+                  <AddToCartWrapper>
+                    <QuantityInput quantity={1} />
+                    <IconButton type="button">
+                      <ShoppingCartSimple size={22} weight="fill" />
+                    </IconButton>
+                  </AddToCartWrapper>
+                </AddToCart>
+              </CoffeeCard>
+            ))}
+          </CoffeeList>
+        </Menu>
       </main>
     </Page>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -104,3 +104,83 @@ export const Benefits = styled.div`
     grid-template-columns: 1fr;
   }
 `;
+
+export const Menu = styled.section`
+  padding: 2rem 10rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.125rem;
+
+  > h1 {
+    font-family: "Baloo 2", sans-serif;
+    font-size: 2rem;
+    line-height: 130%;
+  }
+
+  @media (max-width: 768px) {
+    padding: 1rem 10%;
+  }
+`;
+
+export const CoffeeList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+  }
+`;
+
+export const CoffeeCard = styled.div`
+  width: 16rem;
+  height: 19.375rem;
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px 36px;
+  padding: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  > img {
+    width: 7.5rem;
+    height: 7.5rem;
+    margin-top: -2.5rem;
+  }
+
+  > h1 {
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-family: "Baloo 2", sans-serif;
+    font-weight: 700;
+    font-size: 1.25rem;
+    line-height: 130%;
+    text-align: center;
+  }
+
+  > p {
+    color: ${(props) => props.theme["base-label"]};
+    font-size: 0.875rem;
+    line-height: 130%;
+    text-align: center;
+  }
+`;
+
+export const AddToCart = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0.25rem;
+
+  > span {
+    color: ${(props) => props.theme["base-text"]};
+    font-family: "Baloo 2", sans-serif;
+    font-weight: 800;
+    font-size: 1.5rem;
+    line-height: 130%;
+  }
+`;
+export const AddToCartWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
