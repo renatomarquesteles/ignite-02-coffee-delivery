af6aaee8471e7f3d830bae9e214619363724ef5f
Renato <renatomarquesteles@gmail.com>
2023-03-08 19:02:23 -0300

    chore: adds checkout address fields

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,62 @@
+import { MapPinLine } from "phosphor-react";
+
+import {
+  AddressCard,
+  AddressCardDescription,
+  AddressInputs,
+  AddressInputsWrapper,
+  CartCard,
+  CartInfo,
+  Container,
+  CustomerInfo,
+  PaymentCard,
+  Subtitle,
+} from "./styles";
+
 export const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <Container>
+      <CustomerInfo>
+        <Subtitle>Complete your order</Subtitle>
+
+        <AddressCard>
+          <AddressCardDescription>
+            <MapPinLine size={22} />
+            <div>
+              <h2>Shipping Address</h2>
+              <p>
+                Enter the address where you would like to receive your order
+              </p>
+            </div>
+          </AddressCardDescription>
+
+          <AddressInputs>
+            <AddressInputsWrapper>
+              <input type="text" className="mid" placeholder="ZIP Code" />
+            </AddressInputsWrapper>
+            <AddressInputsWrapper>
+              <input type="text" placeholder="Street" />
+            </AddressInputsWrapper>
+            <AddressInputsWrapper>
+              <input type="text" className="mid" placeholder="Number" />
+              <input type="text" placeholder="Complement" />
+            </AddressInputsWrapper>
+            <AddressInputsWrapper>
+              <input type="text" className="mid" placeholder="Neighborhood" />
+              <input type="text" placeholder="City" />
+              <input type="text" className="short" placeholder="State" />
+            </AddressInputsWrapper>
+          </AddressInputs>
+        </AddressCard>
+
+        <PaymentCard>PaymentMethod</PaymentCard>
+      </CustomerInfo>
+
+      <CartInfo>
+        <Subtitle>Selected coffees</Subtitle>
+
+        <CartCard>Cart</CartCard>
+      </CartInfo>
+    </Container>
   );
 };
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,98 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  padding: 2.5rem 10rem;
+  display: flex;
+  gap: 2rem;
+`;
+
+export const Subtitle = styled.h1`
+  font-family: "Baloo 2", sans-serif;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 130%;
+  color: ${(props) => props.theme["base-subtitle"]};
+`;
+
+export const CustomerInfo = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+const BaseCard = styled.section`
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+  padding: 2.5rem;
+`;
+
+export const AddressCard = styled(BaseCard)`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  svg {
+    color: ${(props) => props.theme["yellow-dark"]};
+    margin-top: 0.375rem;
+  }
+`;
+
+export const AddressCardDescription = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  h2 {
+    color: ${(props) => props.theme["base-subtitle"]};
+    line-height: 130%;
+  }
+
+  p {
+    color: ${(props) => props.theme["base-text"]};
+    font-size: 0.875rem;
+    line-height: 130%;
+    margin-top: 0.125rem;
+  }
+`;
+
+export const AddressInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const AddressInputsWrapper = styled.div`
+  display: flex;
+  gap: 0.75rem;
+
+  input {
+    width: 100%;
+    padding: 0.75rem;
+    background: ${(props) => props.theme["base-input"]};
+    border-radius: 4px;
+    border: 1px solid ${(props) => props.theme["base-button"]};
+    color: ${(props) => props.theme["base-text"]};
+    font-size: 0.875rem;
+    line-height: 130%;
+
+    &.mid {
+      max-width: 12.5rem;
+    }
+
+    &.short {
+      max-width: 3.75rem;
+    }
+  }
+`;
+
+export const PaymentCard = styled(BaseCard)``;
+
+export const CartInfo = styled.div`
+  flex: 1;
+  max-width: 28rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const CartCard = styled(BaseCard)``;
